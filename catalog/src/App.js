96c072b5d3fd9f19b838cdf6ed87dc3dbd63f301
villagef <filwyd123@gmail.com>
2021-01-17 21:41:12 +0100

    navbar

--- catalog/src/App.js
+++ catalog/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from "react";
 import axios from 'axios';
 
-import BreakingBad from "./BreakingBad/BreakingBad"
+import BreakingBad from "./BreakingBad/BreakingBad";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <BreakingBad/>
     </div>
   );
---
+++ catalog/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+export default function Navbar() {
+    return (
+        <div className="navbarWrapper">
+            <div className="logo">
+                <img src="" alt="" srcset=""/>
+            </div>
+        </div>
+    )
+}
